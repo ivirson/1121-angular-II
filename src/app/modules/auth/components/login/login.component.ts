bd73daebf9afe1b957cffd6ca092245d4e7a79c8
Ivirson Daltro <ivi.daltro@gmail.com>
2024-05-20 20:17:58 -0300

    Aula 08 - I18n

--- src/app/modules/auth/components/login/login.component.ts
+++ src/app/modules/auth/components/login/login.component.ts
@@ -52,7 +52,7 @@ export class LoginComponent implements OnInit {
       .subscribe({
         next: (res) => {
           localStorage.setItem(Constants.TOKEN_KEY, `Bearer ${res.token}`);
-          localStorage.setItem(Constants.USER_ROLES, UserRoles.USER);
+          localStorage.setItem(Constants.USER_ROLES, UserRoles.ADMIN);
           this.router.navigate(['']);
         },
         error: (err) => {
